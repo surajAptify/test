980f8a3fc9e4e9afa452972a28b7a71bf5f297fc
suraj sahu <35327354+surajAptify@users.noreply.github.com>
2018-01-15 18:44:09 +0530

    new test

--- peach.php
+++ peach.php
@@ -1,10 +1,7 @@
 <?php 
 echo "welcome";
 echo "suraj";
-<<<<<<< HEAD
 echo "welcome1";
-echo "suraj1";
-=======
-echo "suraj123";
->>>>>>> 531b208e278a67800958ac34b6d90bba75a19807
+echo "suraj1222";
+echo "suraj123333";
 ?>
