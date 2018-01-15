94abd31032487525abc357451820d3de5440e064
Suraj Sahu <Suraj.Sahu@communitybrands.com>
2018-01-15 18:51:09 +0530

    final file

--- peach.php
+++ peach.php
@@ -2,11 +2,7 @@
 echo "welcome";
 echo "suraj";
 echo "welcome14343434";
-<<<<<<< HEAD
 echo "suraj1";
 echo "suraj123";
-=======
-echo "suraj1222";
 echo "suraj123333";
->>>>>>> 980f8a3fc9e4e9afa452972a28b7a71bf5f297fc
 ?>
